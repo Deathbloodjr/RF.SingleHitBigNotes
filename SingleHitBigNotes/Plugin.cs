c244087e0afd8e4a99f86acf38010733b0140743
Deathblood <deathbloodjr@gmail.com>
2024-09-03 20:18:07 -0500

    Initial Commit

---
+++ SingleHitBigNotes/Plugin.cs
@@ -0,0 +1,61 @@
+﻿using BepInEx.Unity.IL2CPP.Utils;
+using BepInEx.Unity.IL2CPP;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using BepInEx.Configuration;
+using SingleHitBigNotes.Plugins;
+
+namespace SingleHitBigNotes
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, ModName, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BasePlugin
+    {
+        const string ModName = "SingleHitBigNotes";
+
+        public static Plugin Instance;
+        private Harmony _harmony;
+        public new static ManualLogSource Log;
+
+
+        public ConfigEntry<bool> ConfigEnabled;
+
+
+
+        public override void Load()
+        {
+            Instance = this;
+
+            Log = base.Log;
+
+            SetupConfig();
+            SetupHarmony();
+        }
+
+        private void SetupConfig()
+        {
+            var dataFolder = Path.Combine("BepInEx", "data", ModName);
+
+            ConfigEnabled = Config.Bind("General",
+                "Enabled",
+                true,
+                "Enables the mod.");
+        }
+
+        private void SetupHarmony()
+        {
+            // Patch methods
+            _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+
+            if (ConfigEnabled.Value)
+            {
+                _harmony.PatchAll(typeof(SingleHitBigNotesPatch));
+                Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is loaded!");
+            }
+            else
+            {
+                Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_NAME} is disabled.");
+            }
+        }
+    }
+}
---
+++ SingleHitBigNotes/Plugins/SingleHitBigNotesPatch.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleHitBigNotes.Plugins
+{
+    internal class SingleHitBigNotesPatch
+    {
+        static bool isRankedMatch = false;
+
+        [HarmonyPatch(typeof(EnsoInput))]
+        [HarmonyPatch(nameof(EnsoInput.GetLastInputForCore))]
+        [HarmonyPatch(MethodType.Normal)]
+        [HarmonyPrefix]
+        public static bool EnsoInput_GetLastInputForCore_Prefix(EnsoInput __instance, ref TaikoCoreTypes.UserInputType __result, int player)
+        {
+            //Plugin.Log.LogInfo("");
+            //Plugin.Log.LogInfo("EnsoInput_GetLastInputForCore_Prefix");
+
+            TaikoCoreTypes.UserInputType NewGetLastInputForCore()
+            {
+                EnsoInput.EnsoInputFlag flag = __instance.GetLastInput(player);
+                if (flag == EnsoInput.EnsoInputFlag.DonR ||
+                    flag == EnsoInput.EnsoInputFlag.DonL ||
+                    flag == EnsoInput.EnsoInputFlag.DaiDon)
+                {
+                    flag = EnsoInput.EnsoInputFlag.DaiDon;
+                }
+                else if (flag == EnsoInput.EnsoInputFlag.KatsuR ||
+                         flag == EnsoInput.EnsoInputFlag.KatsuL ||
+                         flag == EnsoInput.EnsoInputFlag.DaiKatsu)
+                {
+                    flag = EnsoInput.EnsoInputFlag.DaiKatsu;
+                }
+                if (__instance.ensoParam.EnsoEndType == EnsoPlayingParameter.EnsoEndTypes.OptionPerfect || __instance.ensoParam.EnsoEndType == EnsoPlayingParameter.EnsoEndTypes.OptionTraining)
+                {
+                    flag = EnsoInput.EnsoInputFlag.None;
+                }
+                return __instance.ToUserInputType(flag);
+            }
+
+            // I hope this works as intended
+            // This is meant to prevent this mod from working in ranked matches, to avoid being considered cheating
+            // Regular online matches are fine, since those are just for fun
+            if (__instance.ensoParam.networkGameMode != Scripts.EnsoGame.Network.NetworkGameMode.RankMatch)
+            {
+                __result = NewGetLastInputForCore();
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
